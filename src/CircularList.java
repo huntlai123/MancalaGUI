e1701a9418606703ed4d88cf231a4f80a0fe586a
Skrumpcious <iamfes007@aol.com>
2018-04-29 18:31:34 -0700

    Updated JavaDoc
    added pre/postcondition

--- src/CircularList.java
+++ src/CircularList.java
@@ -9,7 +9,9 @@ public class CircularList<E> extends ArrayList<E>{
     /**
      * Allows the elements to be accessed sequentially
      * @param index element to be accessed
-     * @return true element that is being accessed
+     * @return actual element that is being accessed
+     * Precondition: index should be a positive real number
+     * Postcondition: an element contained in the data structure will be returned
      */
     @Override
     public E get(int index)
