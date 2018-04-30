8365b04d951f84a1b3607bd38068323047486fa9
Skrumpcious <iamfes007@aol.com>
2018-04-29 18:36:13 -0700

    Added JavaDoc

--- src/BoardStyle.java
+++ src/BoardStyle.java
@@ -1,7 +1,18 @@
 import java.awt.Graphics2D;
 
+/**
+ * Style interface that sets requirements for the rest of the styles that will 
+ * implement it.
+ */
 public interface BoardStyle
 {
+    /**
+     * Draw method to be overriden by implementing class. 
+     * @param g Graphics2D object that will draw the individual components
+     * @param width used to determine the size of the components along with spacing within
+     * @param height used to determine the size of the components along with spacing within
+     * @return Pit array that will be drawn to the panel or frame
+     */
     public Pit[] draw(Graphics2D g, int width, int height);
 }
 
