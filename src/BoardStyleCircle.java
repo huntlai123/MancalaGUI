2081d442766f73f4d3dff1d5d55be11d738dc934
Skrumpcious <iamfes007@aol.com>
2018-04-29 18:20:38 -0700

    Added JavaDoc
    Updated with JavaDoc comments

--- src/BoardStyleCircle.java
+++ src/BoardStyleCircle.java
@@ -1,10 +1,23 @@
 import java.awt.*;
 import java.awt.geom.*;
 
+/**
+ * Represents a style for the Mancala program that uses circle components for the pit.
+ */
 public class BoardStyleCircle implements BoardStyle
 {
     private Color c;
     
+    /**
+     * Draw method that will be called to draw the circle pits into the frame. It specifies the size of the circles 
+     * that will be drawn and the relative sizing to each other.
+     * @param g Graphics2D object that draws the circle components
+     * @param width used throughout this style to ensure consistent sizing
+     * @param height used throughout this style to ensure consistent sizing
+     * @return an array of pits to be drawn
+     * Precondition: width and height are positive integer values and g is a non-null Graphics2D object
+     * Postcondition: an array of pit objects will be returned with a working draw method to draw the pits.
+     */
     public Pit[] draw(Graphics2D g, int width, int height)
     {
         double diameter = (width * .73)/8;
@@ -34,11 +47,19 @@ public class BoardStyleCircle implements BoardStyle
         return pits;
     }
         
+    /**
+     * Gets the border color of the circle pits.
+     * @return color of the border
+     */
     public Color getBorderColor()
     {
         return Color.LIGHT_GRAY;
     }
     
+    /**
+     * Gets the color that will fill the circle pits.
+     * @return color of the pit
+     */
     public Color getFillColor()
     {
         return new Color(153, 79, 0);
