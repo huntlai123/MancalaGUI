c19476b319e8f122ace13c4615a12b564bacc67a
Hunter Lai <huntlai123@gmail.com>
2018-05-03 13:32:44 -0700

    small changes

--- src/Board.java
+++ src/Board.java
@@ -135,7 +135,7 @@ public class Board {
         			{
         				undoCounter1--;
         			}
-        			changePlayerTurn();
+                    changePlayerTurn();
         		}
         		notifyListeners();
         	}
